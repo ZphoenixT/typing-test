fd5c3566147442f1d0d90d9c6bf53b66fa845b03
Zander Toews <zptoews@gmail.com>
2023-12-01 13:39:11 -0600

    new array is made when restarting

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,7 +1,39 @@
 'use strict';
 
-const words = [
-    'dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building',
+// const words = [
+//     'dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building',
+//     'population', 'weather', 'bottle', 'history', 'dream', 'character', 'money',
+//     'absolute', 'discipline', 'machine', 'accurate', 'connection', 'rainbow',
+//     'bicycle', 'eclipse', 'calculator', 'trouble', 'watermelon', 'developer',
+//     'philosophy', 'database', 'periodic', 'capitalism', 'abominable',
+//     'component', 'future', 'pasta', 'microwave', 'jungle', 'wallet', 'canada',
+//     'coffee', 'beauty', 'agency', 'chocolate', 'eleven', 'technology', 'promise',
+//     'alphabet', 'knowledge', 'magician', 'professor', 'triangle', 'earthquake',
+//     'baseball', 'beyond', 'evolution', 'banana', 'perfume', 'computer',
+//     'management', 'discovery', 'ambition', 'music', 'eagle', 'crown', 'chess',
+//     'laptop', 'bedroom', 'delivery', 'enemy', 'button', 'superman', 'library',
+//     'unboxing', 'bookstore', 'language', 'homework', 'fantastic', 'economy',
+//     'interview', 'awesome', 'challenge', 'science', 'mystery', 'famous',
+//     'league', 'memory', 'leather', 'planet', 'software', 'update', 'yellow',
+//     'keyboard', 'window', 'beans', 'truck', 'sheep', 'band', 'level', 'hope',
+//     'download', 'blue', 'actor', 'desk', 'watch', 'giraffe', 'brazil', 'mask',
+//     'audio', 'school', 'detective', 'hero', 'progress', 'winter', 'passion',
+//     'rebel', 'amber', 'jacket', 'article', 'paradox', 'social', 'resort', 'escape'
+//    ];
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+let randomizedWords = generateNewArray();
+
+function generateNewArray() {
+    const words = [
+        'dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building',
     'population', 'weather', 'bottle', 'history', 'dream', 'character', 'money',
     'absolute', 'discipline', 'machine', 'accurate', 'connection', 'rainbow',
     'bicycle', 'eclipse', 'calculator', 'trouble', 'watermelon', 'developer',
@@ -19,18 +51,46 @@ const words = [
     'download', 'blue', 'actor', 'desk', 'watch', 'giraffe', 'brazil', 'mask',
     'audio', 'school', 'detective', 'hero', 'progress', 'winter', 'passion',
     'rebel', 'amber', 'jacket', 'article', 'paradox', 'social', 'resort', 'escape'
-   ];
+    ];
 
-   function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
+    return shuffleArray([...words]); // Create a copy of the original array and shuffle it
 }
 
-const randomizedWords = shuffleArray(words);
+function displayFirstWord() {
+    const firstWord = randomizedWords[0];
+    const wordDisplay = document.querySelector('.theWord');
+    wordDisplay.textContent = firstWord;
+}
 
+function checkInput() {
+    const typingArea = document.querySelector('.typingArea');
+    const userTypedWord = typingArea.value.trim().toLowerCase();
+    const wordDisplay = document.querySelector('.theWord');
+    const displayedWord = wordDisplay.textContent.toLowerCase();
+
+    if (userTypedWord === displayedWord) {
+        // Add 1 to the score (assuming the score is displayed in the element with class 'score')
+        const scoreElement = document.querySelector('.score');
+        const currentScore = parseInt(scoreElement.textContent.split('/')[0]);
+        const updatedScore = currentScore + 1;
+        scoreElement.textContent = `${updatedScore}/120`;
+
+        // Splice the first word in the array
+        randomizedWords.splice(0, 1);
+
+        // Clear the input field
+        typingArea.value = '';
+
+        // Display the next word
+        if (randomizedWords.length > 0) {
+            displayFirstWord();
+        } else {
+            wordDisplay.textContent = 'No more words';
+        }
+    } else {
+        alert('Words do not match. Try again!');
+    }
+}
 
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -45,6 +105,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function startTimer() {
         if (!isTimerRunning) {
+            displayFirstWord();
             timerInterval = setInterval(() => {
                 timerValue--;
                 timerDisplay.textContent = timerValue;
@@ -66,7 +127,29 @@ document.addEventListener("DOMContentLoaded", function() {
         timerDisplay.textContent = timerValue;
         isTimerRunning = false;
         startBtn.disabled = false; // Enable the Start button on reset
+        
+        // Clear input field
+        const typingArea = document.querySelector('.typingArea');
+        typingArea.value = '';
+    
+        // Reset the displayed word
+        const wordDisplay = document.querySelector('.theWord');
+        wordDisplay.textContent = 'Word Appears Here';
+
+        // Reset the score
+        const scoreElement = document.querySelector('.score');
+        scoreElement.textContent = '0/120';
+
+        // Generate a new shuffled array
+        randomizedWords = generateNewArray();
     }
+
+    const typingArea = document.querySelector('.typingArea');
+    typingArea.addEventListener('keypress', function(event) {
+        if (event.key === 'Enter') {
+            checkInput();
+        }
+    });
 
     startBtn.disabled = true; // Disable Start button initially
     startBtn.addEventListener('click', startTimer);
